160f18de15346665f82c7f8fd2c20c000b9fb942
jiangxin0211 <jiangxin0211@163.com>
2017-07-28 15:27:40 +0800

    20170728

---
+++ require/sort.js
@@ -0,0 +1,13 @@
+define(["isarray"],function(isArray){
+    function sort(arr){
+        if(isArray(arr)){
+            return arr.sort(function(a,b){
+                return a - b ;
+            });
+        }else{
+            return "传入的不是数组";
+        }
+    }
+    return sort;
+});
+
